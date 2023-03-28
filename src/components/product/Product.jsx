6405b0cbc119da20d4ca7f25c426f33ec05c159d
irfansajal <irfan@Irfans-MacBook-Pro.local>
2023-03-28 08:16:29 +0200

    product datail

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './Product.css'
 const Product = (props) => {
    // console.log(props.Product)
-   const {img,name,seller, quantity,price} = props.product;
+   const {img,name,seller, ratings,price} = props.product;
     return (
         <div className='product'>
        <img src={img}/>
-       <h6>{name}</h6>
-
+      <div className='product-info'>
+      <h6 className='product-name'>{name}</h6>
+       <p>Price:${price}</p>
+       <p>Manufacturer:{seller}</p>
+       <p>Ratings:{ratings} stars</p>
+      </div>
+       <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
